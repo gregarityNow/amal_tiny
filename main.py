5ef78cefb4e64a0b46ab97a50d09dce09e5ec2fb
felix <felix.herron@gmail.com>
2023-02-11 15:23:22 +0100

    runtype

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ parser.add_argument("-ro",type=float,default=0.5);
 parser.add_argument("-lr",type=float,default=0.01);
 parser.add_argument("-comp_n_epochs",type=int,default=50);
 parser.add_argument("-n_epochs",type=int,default=50);
-parser.add_argument("-dsName",type=str,default="mnist");
+parser.add_argument("-dsName",type=str,default="mnist cifar10");
 parser.add_argument("-outPath",type=str,default="./../out/");
 parser.add_argument("-runType",type=str,default="train");
 
 opt = parser.parse_args()
 
-if opt.runType == "train":
-	for dsName in opt.dsName.split(" "):
-		train_ViT(opt, dsName)
-
-elif opt.runType == "viz":
-	allLosses, allAccs, compRates, allHiddenSizes = loadRunData(opt)
-	visualize_loss_acc(opt, allLosses, allAccs, compRates)
+for dsName in opt.dsName.split(" "):
+	opt.dsName = dsName
+	if opt.runType == "train":
+		train_ViT(opt)
+	elif opt.runType == "viz":
+		allLosses, allAccs, compRates, allHiddenSizes = loadRunData(opt)
+		visualize_loss_acc(opt, allLosses, allAccs, compRates)
 
 
 
