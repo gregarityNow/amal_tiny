f00c34c346744fb1cc315a5ea6c94244b7dac359
felix <felix.herron@gmail.com>
2023-02-11 23:26:08 +0100

    runtype

--- src/load_data.py
+++ src/load_data.py
@@ -21,21 +21,33 @@ class IncreaseDim:
 
 kwargs = {'num_workers': 10, 'pin_memory': True} if use_cuda else {}
 
-def get_mnist_loaders(batch_size=128, quickie=-1):
+def getRoot(opt):
+	if opt.ppti:
+		root = "/tmp/f002nb9/datasets/mnist"
+		pathlib.Path(root).mkdir(exist_ok=True,parents=True);
+	else:
+		root = "/home/fherron/sac/tina/data"
+		pathlib.Path(root).mkdir(exist_ok=True, parents=True);
+
+	return root
+
+def get_mnist_loaders(opt):
 	transform = transforms.Compose([transforms.ToTensor(),
 									transforms.Normalize((0.1307,), (0.3081,)),
 									IncreaseDim()
 									])
 
-	train_dataset = datasets.MNIST('data', train=True, download=True, transform=transform)
-	test_dataset = datasets.MNIST('data', train=False, download=True, transform=transform)
+	root = getRoot(opt);
+	train_dataset = datasets.MNIST(root=root, train=True, download=True, transform=transform)
+	test_dataset = datasets.MNIST(root=root, train=False, download=True, transform=transform)
 
-	if quickie > -1:
-		train_dataset = torch.utils.data.Subset(train_dataset, torch.arange(quickie))
-		test_dataset = torch.utils.data.Subset(test_dataset, torch.arange(quickie))
 
-	train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, **kwargs)
-	test_loader = DataLoader(test_dataset, batch_size=batch_size, **kwargs)
+	if opt.quickie > -1:
+		train_dataset = torch.utils.data.Subset(train_dataset, torch.arange(opt.quickie))
+		test_dataset = torch.utils.data.Subset(test_dataset, torch.arange(opt.quickie))
+
+	train_loader = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True, **kwargs)
+	test_loader = DataLoader(test_dataset, batch_size=opt.batch_size, **kwargs)
 
 	return train_loader, test_loader, 10
 
@@ -45,8 +57,9 @@ def get_cifar10_loaders(batch_size=128, quickie=-1):
 									transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
 									])
 
-	train_dataset = datasets.CIFAR10('data', train=True, download=True, transform=transform)
-	test_dataset = datasets.CIFAR10('data', train=False, download=True, transform=transform)
+	root = getRoot(opt);
+	train_dataset = datasets.CIFAR10(root, train=True, download=True, transform=transform)
+	test_dataset = datasets.CIFAR10(root, train=False, download=True, transform=transform)
 
 	if quickie > -1:
 		train_dataset = torch.utils.data.Subset(train_dataset, torch.arange(quickie))
