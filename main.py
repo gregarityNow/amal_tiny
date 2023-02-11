da3674159194ac956d00d43b285b59be05e277f7
felix <felix.herron@gmail.com>
2023-02-11 03:13:01 +0100

    nicified the code

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ parser.add_argument("-batch_size",type=int,default=16);
 parser.add_argument("-targetCompRate",type=int,default=32);
 parser.add_argument("-hid_size",type=int,default=50);
 parser.add_argument("-ro",type=float,default=0.5);
+parser.add_argument("-lr",type=float,default=0.01);
 parser.add_argument("-comp_n_epochs",type=int,default=50);
 parser.add_argument("-n_epochs",type=int,default=50);
 parser.add_argument("-dsName",type=str,default="mnist");
