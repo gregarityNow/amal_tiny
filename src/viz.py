215a2d15a1288ddfa92faeff68b597473f6b4761
felix <felix.herron@gmail.com>
2023-02-14 02:22:54 +0100

    enum

--- src/viz.py
+++ src/viz.py
@@ -98,8 +98,8 @@ def illustrate_layer_shrink(opt):
 			for layerIndex in range(len(intermediate)):
 				for subLayerIndex, subLayer in enumerate([intermediate, output]):
 					num = subLayer[layerIndex]
-					width = (60-num)/50
-					xPos = compIndex + width/2
+					width = num/60
+					xPos = compIndex + 1.2*width/2
 
 					yPos = layerIndex + subLayerIndex*0.5
 					height = 0.33
